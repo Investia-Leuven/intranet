3ae698e858bf607c8a930bac3cf3430def5779ae
Vince Coppens <vince.coppens@gmail.com>
2026-04-27 17:16:22 +0200

    maintenance

--- scratch/test_onclick.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.markdown("""
-<button onclick="alert('Hello from HTML!')">Click Me (HTML)</button>
-""", unsafe_allow_html=True)
-
-if st.button("Click Me (Streamlit)"):
-    st.write("Streamlit button clicked")
